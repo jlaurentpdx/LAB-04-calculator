8476ac0fc824dbe8e6fa0542cc6922a5920af2f7
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-08-26 14:53:18 -0700

    Created UI for add

---
+++ calculations.js
@@ -0,0 +1,17 @@
+export function add(x, y) {return x + y;}
+
+export function subtract(x, y) {return x - y;}
+
+export function multiply(x, y) {return x * y;}
+
+export function divide(x, y) {return x / y;}
+
+export function modulo(x, y) {return x % y;}
+
+export function intDivide(x, y) {return Number(x / y).toFixed(0);}
+
+export function exponent(x, y) {return x ** y;}
+
+export function pythagResult(x, y) {return (x * x) + (y * y);}
+
+export function pythagFindC(x, y) {return Number(Math.sqrt(pythagResult(x, y)).toFixed(2));}
