5dbfb28bcd0b5892a0e80a0c083cf42cd1f861e3
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-08-26 17:31:58 -0700

    Working add feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 // import functions
 // import * as Calculate from './calculations.js';
+import { add } from './calculations.js';
 
 // reference needed DOM elements
 const numInput1 = document.getElementById('num-input-1'); 
@@ -8,12 +9,24 @@ const numInput2 = document.getElementById('num-input-2');
 const equalsBtn = document.getElementById('equals-btn');
 const printResult = document.getElementById('print-result');
 
-console.log('', numInput1, '\n', numInput2, '\n', equalsBtn, '\n', printResult);
-
 // set event listeners
 equalsBtn.addEventListener('click', () => {
-    console.log('Add button clicked.');
-});
     // get user input(s)
+    const x = Number(numInput1.value);
+    const y = Number(numInput2.value);
+
     // do any needed work with the value(s)
+    const sum = add(x, y);
+    
     // update DOM to reflect new value(s)
+    printResult.textContent = sum;
+});
+
+//Console log history:
+//
+// console.log('', numInput1, '\n', numInput2, '\n', equalsBtn, '\n', printResult);
+// console.log('Add button clicked.');
+// console.log(add);
+// console.log(x, y);
+// console.log(sum); 
+//
