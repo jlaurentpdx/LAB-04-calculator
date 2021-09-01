c4ccfa019348b8a8885f9b153326363e54b50009
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-09-01 15:53:40 -0700

    Initial commit

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
 /* eslint-disable no-console */
 // import functions
-// import * as Calculate from './calculations.js';
-import { add } from './calculations.js';
+import * as Calculate from './calculations.js';
 
 // reference needed DOM elements
 const numInput1 = document.getElementById('num-input-1'); 
 const numInput2 = document.getElementById('num-input-2'); 
 const equalsBtn = document.getElementById('equals-btn');
 const printResult = document.getElementById('print-result');
+const operatorActive = document.getElementById('operator-active');
+const operatorList = [];
+
+for (let i = 0; i < 9; i++){
+    operatorList[i] = document.getElementById(String(i));
+    operatorList[i].addEventListener('click', () => {
+        operatorActive.textContent = operatorList[i].name;
+        operatorActive.name = operatorList[i].name;
+    });
+}
 
 // set event listeners
 equalsBtn.addEventListener('click', () => {
     // get user input(s)
     const x = Number(numInput1.value);
     const y = Number(numInput2.value);
+    const operatorToResult = operatorActive.name;
+    let result = 0; 
 
     // do any needed work with the value(s)
-    const sum = add(x, y);
+    switch (operatorToResult) {
+        case '+':
+            result = Calculate.add(x, y);
+            break;
+        case '-':
+            result = Calculate.subtract(x, y);
+            break;
+        case 'x':
+            result = Calculate.multiply(x, y);
+            break;
+        case '/d':
+            result = Calculate.divide(x, y);
+            break;
+        case '**':
+            result = Calculate.exponent(x, y);
+            break;
+        case 'c2':
+            result = Calculate.pythagFindC(x, y);
+            break;
+        case 'H':
+            result = Calculate.pythagFindH(x, y);
+            break;
+        case '/i':
+            result = Calculate.intDivide(x, y);
+            break;
+        case '%':
+            result = Calculate.modulo(x, y);
+            break;
+        default:
+            result = Calculate.add(x, y);
+            break;
+    }
     
     // update DOM to reflect new value(s)
-    printResult.textContent = sum;
+    printResult.textContent = result;
 });
+
+
 
 //Console log history:
 //
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { add } from '../calculations.js';
+import * as Calculate from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -11,9 +11,27 @@ test('add function', (expect) => {
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = add(5, 6); // use your function here
+    const actual = Calculate.add(5, 6); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('subtract function', (expect) => {
+    const expected = -1; 
+    const actual = Calculate.subtract(5, 6); // use your function here
+    expect.equal(actual, expected);
+});
+
+test('multiply function', (expect) => {
+    const expected = 27; 
+    const actual = Calculate.multiply(3, 9); // use your function here
+    expect.equal(actual, expected);
+});
+
+test('divide function', (expect) => {
+    const expected = 3.5; 
+    const actual = Calculate.divide(7, 2); // use your function here
     expect.equal(actual, expected);
 });
