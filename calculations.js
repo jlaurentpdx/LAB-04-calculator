401d277bc58ecda1271ab8eb51618be2e46f4241
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-09-01 15:53:40 -0700

    Initial commit

--- calculations.js
+++ calculations.js
@@ -1,18 +1,35 @@
-// eslint-disable-next-line no-unused-vars
 export function add(x, y) {return x + y;}
 
-// export function subtract(x, y) {return x - y;}
+export function subtract(x, y) {return x - y;}
 
-// export function multiply(x, y) {return x * y;}
+export function multiply(x, y) {return x * y;}
 
-// export function divide(x, y) {return x / y;}
+export function divide(x, y) {
+    if (x === 0 || y === 0) {
+        return 'ERROR: division by 0'; 
+    } else {
+        return x / y;
+    }
+}
 
-// export function modulo(x, y) {return x % y;}
+export function modulo(x, y) {    
+    if (x === 0 || y === 0) {
+        return 'ERROR: division by 0'; 
+    } else {
+        return x % y;
+    }
+}
 
-// export function intDivide(x, y) {return Number(x / y).toFixed(0);}
+export function intDivide(x, y) {
+    if (x === 0 || y === 0) {
+        return 'ERROR: division by 0'; 
+    } else {
+        return Number(x / y).toFixed(0);
+    }
+}
 
-// export function exponent(x, y) {return x ** y;}
+export function exponent(x, y) {return x ** y;}
 
-// export function pythagResult(x, y) {return (x * x) + (y * y);}
+export function pythagFindH(x, y) {return (x * x) + (y * y);}
 
-// export function pythagFindC(x, y) {return Number(Math.sqrt(pythagResult(x, y)).toFixed(2));}
+export function pythagFindC(x, y) {return Number(Math.sqrt(pythagFindH(x, y)).toFixed(2));}
