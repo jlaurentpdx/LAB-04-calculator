79b20570b9fc8cfc663a00053bb51fa54902f8e1
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-01 12:59:39 -0700

    updated interface color scheme

--- calculations.js
+++ calculations.js
@@ -24,12 +24,16 @@ export function intDivide(x, y) {
     if (x === 0 || y === 0) {
         return 'ERROR: division by 0'; 
     } else {
-        return Number(x / y).toFixed(0);
+        return Math.floor(Number(x / y).toFixed(0));
     }
 }
 
 export function exponent(x, y) {return x ** y;}
 
-export function pythagFindH(x, y) {return (x * x) + (y * y);}
+export function pythagFindH(x, y) {
+    return (x * x) + (y * y);
+}
 
-export function pythagFindC(x, y) {return Number(Math.sqrt(pythagFindH(x, y)).toFixed(2));}
+export function pythagFindC(x, y) {
+    return Number(Math.sqrt(pythagFindH(x, y)).toFixed(2));
+}
